88fd2964b210c5c3dc96f85fe9f8ab77fd84bb07
anastassia11 <114300806+anastassia11@users.noreply.github.com>
2023-06-28 00:51:55 +0700

    feat: dashboard created

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,55 +1,85 @@
-import { useEffect, useRef, useState } from "react"
+import Header from '@/components/header/Header'
+import geom_preview from '@/../public/geom_preview.jpg'
+import Image from 'next/image'
+import SvgSelector from '@/components/svg/SvgSelector'
 
 export default function DashboardPage() {
-    const contextmenuRef = useRef()
-    const contextmenuHandler = useRef()
-    const [isActive, setIsActive] = useState(false)
-    const [position, setPosition] = useState({ x: 0, y: 0 })
-
-    const handlecontextMenu = (e) => {
-        e.preventDefault()
-        const { pageX, pageY } = e
-        setIsActive(true)
-        setTimeout(() => {
-            const rect = contextmenuRef.current.getBoundingClientRect();
-            const x = (pageX + rect.width) > window.innerWidth ? (window.innerWidth - rect.width) : pageX + 2;
-            const y = (pageY + rect.height) > window.innerHeight ? (window.innerHeight - rect.height) : pageY + 2;
-            setPosition({ x, y })
-            contextmenuRef.current.classList.remove("opacity-0")
-            // document.documentElement.classList.add("overflow-hidden")
-        }, 100)
-    }
-
-    const resetToDefault = () => {
-        setIsActive(false)
-        document.documentElement.classList.remove("overflow-hidden")
-    }
-
-    useEffect(() => {
-        document.addEventListener("click", () => resetToDefault())
-        document.addEventListener("contextmenu", (e) => {
-            if (contextmenuHandler.current && !contextmenuHandler.current.contains(e.target)) resetToDefault()
-        })
-    }, [])
-
+    const projects = [
+        {
+            title: "Project name",
+            desc: "Project description",
+            img: <Image src={geom_preview} width={500} height={500} alt={geom_preview}
+                className='w-full h-48 rounded-t-md' />,
+            date: "Jan 4 2022"
+        },
+        {
+            title: "Project name",
+            desc: "Project description",
+            img: <Image src={geom_preview} width={500} height={500} alt={geom_preview}
+                className='w-full h-48 rounded-t-md' />,
+            date: "Jan 4 2022"
+        },
+        {
+            title: "Project name",
+            desc: "Project description",
+            img: <Image src={geom_preview} width={500} height={500} alt={geom_preview}
+                className='w-full h-48 rounded-t-md' />,
+            date: "Jan 4 2022"
+        },
+        {
+            title: "Project name",
+            desc: "Project description",
+            img: <Image src={geom_preview} width={500} height={500} alt={geom_preview}
+                className='w-full h-48 rounded-t-md' />,
+            date: "Jan 4 2022"
+        }
+    ]
 
     return (
-        <main className="px-4">
-            <div ref={contextmenuHandler} onContextMenu={handlecontextMenu} className="max-w-sm h-32 mx-auto mt-12 rounded-lg border border-dashed bg-gray-50 text-sm flex items-center justify-center select-none text-gray-600">
-                <p>Right click here.</p>
-            </div>
-            {
-                isActive ? (
-                    <div ref={contextmenuRef} className="fixed z-10 opacity-0 max-w-[17rem] w-full rounded-lg bg-white shadow-md border text-sm text-gray-800" style={{ top: `${position.y}px`, left: `${position.x}px` }}>
-                        <div className="px-2 py-1.5 border-t" >
-                            <button className="w-full flex items-center justify-between gap-x-2 px-2 py-1.5 hover:text-white hover:bg-blue-600 active:bg-blue-500 rounded-lg duration-150 group cursor-default" role="menuitem">
-                                delete
-                            </button>
+        <div className='w-screen min-h-screen bg-gray-100'>
+            <Header />
+            <div className="px-10 py-5">
+                <div className='flex flex-row justify-between space-x-2'>
+                    <h1 className="text-xl py-2 text-gray-600 font-medium">
+                        Projects
+                    </h1>
+                    <div className='flex flex-row justify-end space-x-2'>
+                        <div className="relative">
+                            <SvgSelector id='search' />
+                            <input type="text" placeholder="Search" className="w-80 h-10 pl-12 pr-4 text-base text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-[#f9a86267]" />
                         </div>
+                        <button className="w-40 px-4 h-10 text-base text-white font-medium bg-[#f98929] hover:bg-[#ff9639] active:bg-[#ff8d29] rounded-lg duration-150">
+                            New Project
+                        </button>
                     </div>
+                </div>
 
-                ) : ""
-            }
-        </main>
+                <section className="">
+
+                    <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4">
+                        {
+                            projects.map((items, key) => (
+                                <article className="max-w-md mx-auto mt-4 bg-white shadow-md border rounded-lg duration-300 hover:shadow-sm cursor-pointer" key={key}>
+                                    <a href={items.href}>
+                                        {items.img}
+                                        <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
+                                            <span className="block text-gray-400 text-sm">{items.date}</span>
+                                        </div>
+                                        <div className="pt-3 ml-4 mr-2 mb-3">
+                                            <h3 className="text-base text-gray-600 font-medium">
+                                                {items.title}
+                                            </h3>
+                                            <p className="text-gray-400 text-sm mt-1">{items.desc}</p>
+                                        </div>
+                                    </a>
+                                </article>
+                            ))
+                        }
+                    </div>
+                </section>
+            </div>
+        </div>
     )
 }
+
+DashboardPage.requiresAuth = true
