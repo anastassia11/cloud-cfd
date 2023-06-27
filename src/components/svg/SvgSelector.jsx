ec552491eb83777ce34c822751d044e3a73383d4
anastassia11 <114300806+anastassia11@users.noreply.github.com>
2023-06-28 00:54:26 +0700

    feat: added 'search' svg  to SvgSelector

--- src/components/svg/SvgSelector.jsx
+++ src/components/svg/SvgSelector.jsx
@@ -55,8 +55,12 @@ export default function SvgSelector({ id = '' }) {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={0.5} stroke="currentColor" class="w-14 h-14 stroke-gray-400">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
-
             )
-
+        case 'search':
+            return (
+                <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-5 h-5 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg >
+            )
     }
 }
