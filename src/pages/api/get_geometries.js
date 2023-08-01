f2c0633ab1648c52364c674ac182dcea1f450cce
anastassia11 <114300806+anastassia11@users.noreply.github.com>
2023-08-01 15:05:29 +0700

    refactor: request to get geometries moved to api

---
+++ src/pages/api/get_geometries.js
@@ -0,0 +1,15 @@
+import { BASE_SERVER_URL } from '@/utils/constants'
+import axios from 'axios'
+
+export default async function getGeometries(idProject) {
+    try {
+        const response = await axios.get(`${BASE_SERVER_URL}/api/Project/Get_Geometries/` + idProject)
+        if (response.status === 200) {
+            return { success: true, data: response.data }
+        } else {
+            throw new Error('Error getting geometries')
+        }
+    } catch (error) {
+        return { success: false, message: error.message }
+    }
+}
