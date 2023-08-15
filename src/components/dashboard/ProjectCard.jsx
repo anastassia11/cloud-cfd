f2bc84dd09163a525f254b644410e80aab90a894
anastassia11 <114300806+anastassia11@users.noreply.github.com>
2023-08-15 14:38:11 +0700

    feat: project menu

--- src/components/dashboard/ProjectCard.jsx
+++ src/components/dashboard/ProjectCard.jsx
@@ -8,31 +8,31 @@ import { useEffect, useState } from 'react';
 
 export default function ProjectCard({ item = {}, onDeleteClick, onEditClick }) {
     const router = useRouter()
-    const [isOpen, setIsOpen] = useState(false)
+    const [position, setPosition] = useState({ x: 0, y: 0 })
 
     const date = new Date(item.updateTime)
     const day = date.getDate()
     const month = date.getMonth()
     const year = date.getFullYear()
+
     const formettedData = `${day < 10 ? '0' : ''}${day}.${month < 10 ? '0' : ''}${month}.${year}`
 
     useEffect(() => {
-        document.addEventListener("click", handleMenuClose);
+        const element = document.getElementById(item.id)
+        const rect = element.getBoundingClientRect()
+        setPosition({ x: rect.x, y: rect.y })
+        const handleMutation = () => {
+            if (element) {
+                const rect = element.getBoundingClientRect()
+                setPosition({ x: rect.x, y: rect.y })
+            }
+        }
+        const observer = new MutationObserver(handleMutation)
+        observer.observe(document.body, { childList: true, subtree: true })
         return () => {
-            document.removeEventListener("click", handleMenuClose);
+            observer.disconnect()
         }
     }, [])
-
-    const handleMenuOpen = () => {
-        console.log('handleMenuOpen')
-        setIsOpen(true)
-    }
-
-    const handleMenuClose = (e) => {
-        if (e.target.tagName !== "BUTTON") {
-            setIsOpen(false)
-        }
-    }
 
     const handleExternalClick = () => {
         router.push(`/workbench/${item.id}`)
@@ -44,49 +44,50 @@ export default function ProjectCard({ item = {}, onDeleteClick, onEditClick }) {
     }
 
     return (
-        <div className="group overflow-hidden rounded-lg shadow transition hover:shadow-lg bg-white flex flex-col">
+        <div className="overflow-hidden rounded-lg shadow transition hover:shadow-lg bg-white flex flex-col">
             <Image src={geom_preview2} width={500} height={500} alt={geom_preview}
                 className='h-56 w-full object-cover' />
-            <div className="bg-white p-4 flex flex-col justify-between flex-grow h-full">
+            <div className="bg-white py-4 pl-4 flex flex-col justify-between flex-grow h-full">
                 <div>
                     <div className='flex flex-row justify-between items-start'>
                         <time datetime={formettedData} className="block text-xs text-gray-500">
                             {`Last modified: ${formettedData}`}
                         </time>
-                        <div className='relative'>
-                            <button className='flex items-end flex-row justify-end group-hover:visible'
-                                onClick={handleMenuOpen}>
-                                <SvgSelector id='more' className='w-[16px] h-[16px] rotate-90' />
-                            </button>
-
-                            <div className="group-hover:visible absolute right-0 z-10 max-w-[200px] w-[200px] rounded-md bg-white shadow border text-day-350">
+                    </div>
+                    <div className='flex flex-row justify-between items-center'>
+                        <p className="mt-0.5 text-lg text-gray-900">
+                            {item.name}
+                        </p>
+                        <button className='group flex flex-row justify-center items-center w-10 h-10' id={item.id}>
+                            <SvgSelector id='more' className='w-[16px] h-[16px] rotate-90' />
+                            <div className="invisible group-hover:visible absolute z-10 max-w-[200px] w-[200px] rounded-md bg-white shadow border
+                             text-day-350"
+                                style={{ top: `${position.y + 30}px`, left: `${position.x - 170}px` }}>
                                 <div className='px-2 py-1.5'>
-                                    <button className="flex w-full items-center gap-2 rounded-md px-2 h-9 text-base text-day-350 hover:bg-day-150"
-                                    >
+                                    <button className="flex w-full items-center gap-2 rounded-md px-2 h-9 text-base text-day-350 
+                                    hover:bg-day-150"
+                                        onClick={handleExternalClick}>
                                         <SvgSelector id='open' />
                                         Open
                                     </button>
-                                    <button className="flex w-full items-center gap-2 rounded-md px-2 h-9 text-base text-day-350 hover:bg-day-150"
-                                    >
+                                    <button className="flex w-full items-center gap-2 rounded-md px-2 h-9 text-base text-day-350 
+                                    hover:bg-day-150">
                                         <SvgSelector id='edit' className='w-5 h-5' />
                                         Edit
                                     </button>
                                 </div>
                                 <div className='px-2 py-1.5 border-t'>
-                                    <button className="flex w-full items-center gap-2 rounded-md px-2 h-9 text-base text-red-700 hover:bg-red-50 "
-                                    >
+                                    <button className="flex w-full items-center gap-2 rounded-md px-2 h-9 text-base text-red-700 
+                                    hover:bg-red-50 "
+                                        onClick={handleDeleteClick}>
                                         <SvgSelector id='delete' className="h-5 w-5" />
                                         Delete
                                     </button>
                                 </div>
                             </div>
-                        </div>
-
+                        </button>
                     </div>
-                    <p className="mt-0.5 text-lg text-gray-900">
-                        {item.name}
-                    </p>
-                    <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                    <p className="line-clamp-3 text-sm/relaxed text-gray-500 mr-4">
                         {item.description}
                     </p>
                 </div>
@@ -95,8 +96,6 @@ export default function ProjectCard({ item = {}, onDeleteClick, onEditClick }) {
                     <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">&rarr;</span>
                 </Link>
             </div>
-
-
         </div >
     )
 }
