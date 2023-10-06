ca791ab67c8ab72698a0a5453201932131ab2cc4
anastassia11 <114300806+anastassia11@users.noreply.github.com>
2023-10-06 15:38:20 +0700

    style: small fixes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -71,7 +71,7 @@ export default function Header() {
                             className='w-5 h-5 duration-300 group-hover:rotate-180'>
                             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                         </svg>
-                        <div className='py-2 bg-white rounded-lg shadow absolute 
+                        <div className='py-2 bg-white rounded-lg shadow absolute invisible group-hover:visible
                             top-[46px] border border-day-200 right-0 w-44 text-base z-20 duration-100 opacity-0 group-hover:opacity-100 
                             ease-linear transition-all group-hover:translate-y-[10px]'>
                             <div className='flex flex-col'>
